eb94fc8c596b01d03229d373d5c5cf4536e7f40d
NatalyHG <naty19972008@hotmail.com>
2020-02-29 18:53:19 -0500

    Upload my code 1rst time

---
+++ src/test/java/Page/PageFormulario.java
@@ -0,0 +1,46 @@
+package Page;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.ITestResult;
+
+import Bean.BeanFormulario;
+import ConfiguracionBase.ConfigBase;
+import Utilitarios.GetScreenShot;
+import Utilitarios.UtilitariosButton;
+import Utilitarios.UtilitariosTextField;
+
+public class PageFormulario extends ConfigBase {
+	public PageFormulario()
+	{
+	String baseUrl = "https://docs.google.com/forms/d/e/1FAIpQLSfzLAD7DNf6fz1tu_gtbuyDZVbpSbeR_PNS3zOtlj416VZ1jA/viewform";
+	PageFactory.initElements(driver, this); // si no se inicializan los WebElement da error de null pointer exception
+	driver.get(baseUrl);
+	}
+	
+	@FindBy(name = "entry.2005620554")
+	WebElement inputnombreApellido;
+	
+	@FindBy(name = "entry.1140037502")
+	WebElement inputcorreo;
+	
+	@FindBy(xpath = "//*[@id=\"mG61Hd\"]/div/div/div[3]/div[1]/div/div/span/span")
+	WebElement btnSiguiente;
+	
+	@FindBy(xpath = "//span[contains(text(),'Enviar')]")
+	WebElement enviar;
+	
+	
+	
+	public void registrarFormulario(ITestResult testResult,BeanFormulario beanFormulario ) throws InterruptedException, IOException, Exception {
+		UtilitariosTextField.txtField_putString(beanFormulario.getNombreApellido(), inputnombreApellido, driver, testResult);
+		UtilitariosTextField.txtField_putString(beanFormulario.getCorreo(), inputcorreo, driver, testResult);
+		UtilitariosButton.btn_clic(btnSiguiente, driver, testResult);
+		UtilitariosButton.btn_clic(enviar, driver, testResult);
+		
+		GetScreenShot.capturarPantallaFile(driver, "-IngresoNombreYCorreo.png");
+	}
+}
